dce1993bbb476c6a02a942d277bf256f804a4815
TravisGuillory <travisguillory70@gmail.com>
2020-05-28 03:25:06 -0500

    added eslint

--- routes/api.js
+++ routes/api.js
@@ -1,32 +1,32 @@
-const router = require("express").Router();
-const Transaction = require("../models/transaction.js");
+const router = require('express').Router();
+const Transaction = require('../models/transaction.js');
 
-router.post("/api/transaction", ({body}, res) => {
+router.post('/api/transaction', ({ body }, res) => {
   Transaction.create(body)
-    .then(dbTransaction => {
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(404).json(err);
     });
 });
 
-router.post("/api/transaction/bulk", ({body}, res) => {
+router.post('/api/transaction/bulk', ({ body }, res) => {
   Transaction.insertMany(body)
-    .then(dbTransaction => {
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(404).json(err);
     });
 });
 
-router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
-    .then(dbTransaction => {
+router.get('/api/transaction', (req, res) => {
+  Transaction.find({}).sort({ date: -1 })
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(404).json(err);
     });
 });
--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+/* eslint-disable no-console */
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const compression = require('compression');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`);
